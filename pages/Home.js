7491540f58afc5e6085242cc62c9faa1eb1c7bf1
Edel Ordoñez <ordonez.e.15@hotmail.com>
2022-11-08 11:00:13 -0600

    Added placeholder styles

--- pages/Home.js
+++ pages/Home.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet } from 'react-native'
 
 function Home() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/icons/home.png')}
+                style={styles.logo}
+            />
+            <Text style={styles.text}>Home</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        marginBottom: 20,
+    },
+    text: {
+        fontSize: 30,
+        color: '#d5145a',
+    },
+})
 
 export default Home
--- pages/Profile.js
+++ pages/Profile.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet } from 'react-native'
 
 const Profile = () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Text>Profile</Text>
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/icons/profile.png')}
+                style={styles.logo}
+            />
+            <Text style={styles.text}>Profile</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        marginBottom: 20,
+    },
+    text: {
+        fontSize: 30,
+        color: '#d5145a',
+    },
+})
 
 export default Profile
